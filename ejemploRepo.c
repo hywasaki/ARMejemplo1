7d316f237c46300eda08465d4e8f3b96c55e5176
hywasaki <a20192507@pucp.edu.pe>
2023-08-24 09:00:24 -0500

    [ADD]  Arreglo de numeros para la insercion crrado

--- ejemploRepo.c
+++ ejemploRepo.c
@@ -8,16 +8,12 @@ int main(){
 
     int numeros_burbuja[100]; // creamos un arreglo de 100 datos
 
-    printf("Arreglo inicial para burbuja: \n")
-    for (int= i ; i < 100; i++)
+    printf("Arreglo inicial para burbuja: \n");
+    for (int i=1 ; i < 100; i++)
     {
         numeros_burbuja[i]=rand()%100; // numeros aleatorios de 0 a 100 en el array 
         printf("%d", numeros_burbuja[i]); // imrpimimos el valor
     }
-    
-
-
-
-
+    return 0;
 
 }
