9ec8ffbf60c96abd2a41dc6656b9c748f410316b
hywasaki <a20192507@pucp.edu.pe>
2023-08-24 09:34:14 -0500

    [ADD] Se han agregado metricas temporales

--- ejemploRepo.c
+++ ejemploRepo.c
@@ -26,6 +26,23 @@ int main(){
         numeros_insercion[i]=rand()%100; // numeros aleatorios de 0 a 100 en el array 
         printf("%d", numeros_insercion[i]); // imrpimimos el valor
     }
+    printf("Ordenamiento burbuja: \n ");
+    clock_t inicio_burbuja =clock(); // devuele la cantidad de ciclos desde que se inicia el programa
+    // aque se ejecutara el algoritmo burbuja
+
+    clock_t fin_burbuja = clock();
+    double tiempo_burbuja=(double)(fin_burbuja-inicio_burbuja)/(CLOCKS_PER_SEC);
+    // CLOCKS_PER_SEC es un constante que tiene la cantidad de cilos que genera nuestro procesador
+    // la division nos da los segundos que se demora en ejecutrar nuestro algortimso
+
+    printf("Ordenamiento insercion: \n ");
+    clock_t inicio_insercion =clock(); // 
+    clock_t fin_insercion = clock();
+    double tiempo_insersion=(double)(fin_insercion-inicio_insercion)/(CLOCKS_PER_SEC);
+
+
+
+
     return 0;
 
 }
